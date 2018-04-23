10139022161eb9e8f11b4509d71331b9b699c8e0
rodger-yuan <yuan.rodger@gmail.com>
2018-04-22 23:22:25 -0400

    added averages, sorting, top players

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function roundFromGameId(gameId) {
 
 $( document ).ready(function() {
 	var url = "https://stats.nba.com/stats/leaguegamelog?Counter=1000000&DateFrom=&DateTo=&Direction=ASC&LeagueID=00&PlayerOrTeam=P&Season=2017-18&SeasonType=Playoffs&Sorter=DATE&callback=?"
+	var url2 = "https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=Totals&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2017-18&SeasonSegment=&SeasonType=Playoffs&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight=&callback=?"
 
 	var rodger_players = ["LeBron James", "Kevin Durant", "Joel Embiid", "Kevin Love", "DeMar DeRozan", "Donovan Mitchell", "CJ McCollum", "Jaylen Brown", "Paul George", "Giannis Antetokounmpo"]
 	var jimmy_players = ["James Harden", "Chris Paul", "Klay Thompson", "Stephen Curry", "Damian Lillard", "Eric Gordon", "Russell Westbrook", "Ben Simmons", "Kyle Lowry", "JJ Redick"]
@@ -20,6 +21,7 @@ $( document ).ready(function() {
 			this._round3points = 0;
 			this._round4points = 0;
 			this._totalpoints = 0;
+			this._average = 0;
 		}
 
 		inputBoxScore(boxline) {
@@ -42,6 +44,8 @@ $( document ).ready(function() {
 					this._round4points += points;
 					break;
 			}
+
+			this._average = Number(this._totalpoints/this._totalgames).toFixed(1);
 		}
 	}
 
@@ -56,10 +60,9 @@ $( document ).ready(function() {
 	$.getJSON(url,  // url
 	    function (data) {  // success callback
 	        rowSet = data.resultSets[0].rowSet;
-	        console.log(rowSet);
-	        rodger_total = new Array(7).fill(0);
+	        rodger_total = new Array(8).fill(0);
 	        rodger_total[0] = "Rodger Totals"
-	        jimmy_total = new Array(7).fill(0);
+	        jimmy_total = new Array(8).fill(0);
 	        jimmy_total[0] = "Jimmy Totals"
 
 	        for (var i = 0; i < rowSet.length; i++) { //input boxscores into players
@@ -71,8 +74,6 @@ $( document ).ready(function() {
 						jimmy_players_class[rowSet[i][2]].inputBoxScore(rowSet[i]);
 					}
 			}
-
-			console.log(jimmy_players_class)
 				
 			for (var i = 0; i < rodger_players.length; i++) {
 				rodger_name = rodger_players[i];
@@ -104,6 +105,7 @@ $( document ).ready(function() {
 				row.insertCell(4).innerHTML = rodger_players_class[rodger_name]._round3points;
 				row.insertCell(5).innerHTML = rodger_players_class[rodger_name]._round4points;
 				row.insertCell(6).innerHTML = rodger_players_class[rodger_name]._totalpoints;
+				row.insertCell(7).innerHTML = rodger_players_class[rodger_name]._average;
 
 				row = document.getElementById("jimmy_total").insertRow(1); //jimmy
 				row.insertCell(0).innerHTML = jimmy_players_class[jimmy_name]._name;
@@ -113,30 +115,50 @@ $( document ).ready(function() {
 				row.insertCell(4).innerHTML = jimmy_players_class[jimmy_name]._round3points;
 				row.insertCell(5).innerHTML = jimmy_players_class[jimmy_name]._round4points;
 				row.insertCell(6).innerHTML = jimmy_players_class[jimmy_name]._totalpoints;
+				row.insertCell(7).innerHTML = jimmy_players_class[jimmy_name]._average;
 			}
 
-			console.log(rodger_total)
+			rodger_total[7] = Number(rodger_total[6]/rodger_total[1]).toFixed(1);
+			jimmy_total[7] = Number(jimmy_total[6]/jimmy_total[1]).toFixed(1);
 
 			for (var i = 0; i < rodger_total.length; i++) {
-				jrow = document.getElementById("jt");
-				rrow = document.getElementById("rt");
-				jrow.insertCell(i).innerHTML = jimmy_total[i];
-				rrow.insertCell(i).innerHTML = rodger_total[i];
+				if (i != 0) {
+					jrow = document.getElementById("jtl");
+					rrow = document.getElementById("rtl");
+					jrow.insertCell(i).innerHTML = jimmy_total[i];
+					rrow.insertCell(i).innerHTML = rodger_total[i];
+				}
 
 				jcell = document.getElementById("jtt").insertCell(i);
 				rcell = document.getElementById("rtt").insertCell(i);
 				jcell.innerHTML = jimmy_total[i];
 				rcell.innerHTML = rodger_total[i];
 
-			console.log(rodger_total);
-			console.log(jimmy_total);
-
 			}
 	    });
+
+	$.getJSON(url2,  // url2
+	    function (data) {  // success callback
+	    	rowSet = data.resultSets[0].rowSet;
+	    	rowSet.sort(function(a,b) {return b[29] - a[29]})
+	    	for (var i = 0; i < 20; i++) {
+	    		row = document.getElementById("topplayers").insertRow(i+1);
+	    		if (rodger_players.indexOf(rowSet[i][1]) > -1) {
+	    			row.className = "rodger";
+	    		}
+	    		if (jimmy_players.indexOf(rowSet[i][1]) > -1) {
+	    			row.className = "jimmy";
+	    		}
+	    		row.insertCell(0).innerHTML = rowSet[i][1];
+	    		row.insertCell(1).innerHTML = rowSet[i][5];
+	    		row.insertCell(2).innerHTML = rowSet[i][29];
+	    		row.insertCell(3).innerHTML = Number(rowSet[i][29]/rowSet[i][5]).toFixed(1);
+	    	}
+	    }
+	);
 });
 
 function scrolly(name) {
-	console.log(name)
     var elmnt = document.getElementById(name);
     elmnt.scrollIntoView();
 }
